aa75c047f54e3d852fe2ea402fd7e0d5e9c2d515
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2019-08-06 23:35:30 +0530

    Cleared comments and small changes made

--- js/try.js
+++ js/try.js
@@ -30,7 +30,7 @@ class Enemy {
     
         if((enemy1xDist < 30 && enemy1yDist < 40) || (enemy2xDist < 30 && enemy2yDist < 40) || (enemy3xDist < 30 && enemy3yDist < 40)){
              alert("hit")
-             restart();
+             player.restart();
             };
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
@@ -58,6 +58,12 @@ class Player {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
     }
 
+    update() {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.    
+    }
+
     handleInput() {
         let z = allowedKeys;
 
@@ -81,84 +87,9 @@ class Player {
     }
 }
 
-// // Update the enemy's position, required method for game
-// // Parameter: dt, a time delta between ticks
-// Enemy.prototype.update = function(dt) {
-//     this.x = this.x + this.speed * dt;
-
-//     if(this.x > 500) { 
-//         this.x = 0;
-//     }
-
-//     const enemy1yDist = Math.abs(player.y - enemy1.y);
-//     const enemy1xDist = Math.abs(player.x - enemy1.x);
-//     const enemy2yDist = Math.abs(player.y - enemy2.y);
-//     const enemy2xDist = Math.abs(player.x - enemy2.x);
-//     const enemy3yDist = Math.abs(player.y - enemy3.y);
-//     const enemy3xDist = Math.abs(player.x - enemy3.x);
-
-
-//     if((enemy1xDist < 30 && enemy1yDist < 40) || (enemy2xDist < 30 && enemy2yDist < 40) || (enemy3xDist < 30 && enemy3yDist < 40)){
-//          alert("hit")
-//          player.restart();
-//         };
-//     // You should multiply any movement by the dt parameter
-//     // which will ensure the game runs at the same speed for
-//     // all computers.
-// };
-
-// Draw the enemy on the screen, required method for game
-// Enemy.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-
-// Now write your own player class
-// var Player = function() {
-//     // Variables applied to each of our instances go here,
-//     // we've provided one for you to get started
-//     // The image/sprite for our enemies, this uses
-//     // a helper we've provided to easily load images
-//     this.sprite = 'images/char-boy.png';
-//     this.x = 200;
-//     this.y = 400;
-// };
-
-// Player.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Player.prototype.handleInput = function(allowedKeys){
-//     let z = allowedKeys;
-
-//     if(z === 'up'){
-//         this.y -= 85;  
-//     }
-//     if(z === 'left'){
-//         this.x -= 85;  
-//     }
-//     if(z === 'right'){
-//         this.x += 85;  
-//     }
-//     if(z === 'down'){
-//         this.y += 85;  
-//     }
-// }
-
-// Player.prototype.restart = function() {
-//     this.x = 200;
-//     this.y = 400;
-// }
-
 let player = new Player();
 // This class requires an update(), render() and
 // a handleInput() method.
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
